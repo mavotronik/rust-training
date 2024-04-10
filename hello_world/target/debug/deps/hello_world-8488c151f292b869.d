85979b0f28244b2a7fddda297e2eafaeddeed808
mavotronik <matvey_popov2004@mail.ru>
2024-04-10 18:55:42 +0300

    update

---
+++ hello_world/target/debug/deps/hello_world-8488c151f292b869.d
@@ -0,0 +1,5 @@
+/home/matvey/rust-training/hello_world/target/debug/deps/hello_world-8488c151f292b869: src/main.rs
+
+/home/matvey/rust-training/hello_world/target/debug/deps/hello_world-8488c151f292b869.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/matvey/rust-training/hello_world/target/debug/hello_world: /home/matvey/rust-training/hello_world/src/main.rs
